03bb7dc42655b446bd7bf680470adc5aaf5ecd60
broskbenny <broskbenny@gmail.com>
2026-09-26 14:00:45 +0200

    Capture flat-ride animation around simulation ticks

--- src/openrct2/entity/EntityTweener.cpp
+++ src/openrct2/entity/EntityTweener.cpp
@@ -153,6 +153,8 @@ namespace OpenRCT2
                 v.spinBefore = v.spinAfter = vehicle->spin_sprite;
                 v.pitchBefore = v.pitchAfter = static_cast<uint8_t>(vehicle->pitch);
                 v.rollBefore = v.rollAfter = static_cast<uint8_t>(vehicle->roll);
+                v.flatPrimaryBefore = v.flatPrimaryAfter = vehicle->flatRideAnimationFrame;
+                v.flatSecondaryBefore = v.flatSecondaryAfter = vehicle->flatRideSecondaryAnimationFrame;
                 _trackedVisuals = v;
             }
         }
@@ -168,6 +170,8 @@ namespace OpenRCT2
                 _trackedVisuals->spinAfter = vehicle->spin_sprite;
                 _trackedVisuals->pitchAfter = static_cast<uint8_t>(vehicle->pitch);
                 _trackedVisuals->rollAfter = static_cast<uint8_t>(vehicle->roll);
+                _trackedVisuals->flatPrimaryAfter = vehicle->flatRideAnimationFrame;
+                _trackedVisuals->flatSecondaryAfter = vehicle->flatRideSecondaryAnimationFrame;
                 _trackedVisuals->alpha = 1.0f;
             }
             else _trackedVisuals.reset();
