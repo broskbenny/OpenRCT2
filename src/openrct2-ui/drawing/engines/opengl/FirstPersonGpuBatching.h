f8afff340cbe2ee05a20d5c6cca3a043bcbde839
broskbenny <broskbenny@gmail.com>
2026-09-26 09:42:36 +0200

    Fingerprint immutable first-person bitmap content

--- src/openrct2-ui/drawing/engines/opengl/FirstPersonGpuBatching.h
+++ src/openrct2-ui/drawing/engines/opengl/FirstPersonGpuBatching.h
@@ -42,6 +42,7 @@ namespace OpenRCT2::Ui
         ExtendFirstPersonFingerprint(fingerprint,s.mask.HasValue() ? FirstPersonMaterialFingerprint(s.mask) : 0);
         ExtendFirstPersonFingerprint(fingerprint,s.depthBias ? 1u : 0u);
         ExtendFirstPersonFingerprint(fingerprint,s.edgeCoverage ? 1u : 0u);
+        ExtendFirstPersonFingerprint(fingerprint,s.immutableFingerprint);
         for (const auto& v : s.triangles)
         {
             ExtendFirstPersonFingerprint(fingerprint,std::bit_cast<uint32_t>(v.world.x));
